4ef8cf0f872b303926aa16a70d60b8c231377e12
LaminatedWaffle67 <laminatedwaffle67@gmail.com>
2025-05-12 08:03:51 +1000

    "Completed OS detection and global path sharing"

--- config.py
+++ config.py
@@ -21,17 +21,39 @@ if user_sys == "Windows":
     if os.path.exists(image_directory_path):
         listed_image_files = os.listdir(image_directory_path)
 
-        for image in listed_image_files:
-            image_file_path = 
+        for image_path in listed_image_files:
+            image_file_path = os.path.join(image_directory_path, image_path)
             image_list.append(image_file_path)
 
 elif user_sys == "Darwin":
     username = os.getenv("USER")
     directory_path = f"\home\{username}\Desktop\TaipanImgFx"
+    image_folder_path = "TaipanCode\Fileman\Display\images"
+
+    image_directory_path = os.path.join(directory_path, image_folder_path)
+
+    if os.path.exists(image_directory_path):
+        listed_image_files = os.listdir(image_directory_path)
+
+        for image_path in listed_image_files:
+            image_file_path = os.path.join(image_directory_path, image_path)
+            image_list.append(image_file_path)
+
 
 elif user_sys == "Linux":
     username = os.getenv("USER")
     directory_path = f"\home\{username}\Desktop\TaipanImgFx"
+    image_folder_path = "TaipanCode\Fileman\Display\images"
+
+    image_directory_path = os.path.join(directory_path, image_folder_path)
+
+    if os.path.exists(image_directory_path):
+        listed_image_files = os.listdir(image_directory_path)
+
+        for image_path in listed_image_files:
+            image_file_path = os.path.join(image_directory_path, image_path)
+            image_list.append(image_file_path)
+
 
 else:
     raise("Uknown user OS")
