4d9246b766a21f719bd87ee5010db9e1f185f232
LaminatedWaffle67 <laminatedwaffle67@gmail.com>
2025-05-11 12:05:45 +1000

    "started adding OS detection in config"

--- config.py
+++ config.py
@@ -3,19 +3,24 @@ import os, platform
 
 image_count = 15
 
-UserSys = platform.system()
+user_sys = platform.system()
+username = None
+directory_path = None
+folder_path = None
+image_file_path = None
 
-if UserSys == "Windows":
-    Username = os.getenv("USERNAME")
+if user_sys == "Windows":
+    username = os.getenv("USERNAME")
+    directory_path = fr"C:\Users\{username}\Desktop\taipan"
 
-elif UserSys == "Darwin":
-    Username = os.getenv("USER")
+elif user_sys == "Darwin":
+    username = os.getenv("USER")
+    directory_path = fr"C:\Users\{username}\Desktop\taipan"
 
-elif UserSys == "Linux":
-    Username = os.getenv("USER")
+elif user_sys == "Linux":
+    username = os.getenv("USER")
+    directory_path = fr"C:\Users\{username}\Desktop\taipan"
 
 else:
     raise("Uknown user OS")
 
-folder_path = ""
-image_file_path = ""
